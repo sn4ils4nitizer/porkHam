4cf6f71bf5b21903e94b6bc6e2dfd0170b213cdc
sn4ils4nitizer <filipbuczynski2@gmail.com>
2025-05-26 12:48:02 -0500

    wave file sent as part of response now

--- handlers/oracleQuery.go
+++ handlers/oracleQuery.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
 )
 
@@ -15,12 +17,21 @@ func QueryHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	cmd := exec.Command("./oracleQuery.pl", string(prompt))
-	output, err := cmd.CombinedOutput()
-	if err != nil {
+	if err := cmd.Run(); err != nil {
 		http.Error(w, "Oracle Query Command Failed."+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	w.Write(output)
+	audiodata, err := os.ReadFile("output.wav")
+	if err != nil {
+		http.Error(w, "Failed to fetch audio:"+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "audio/wave")
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(audiodata)))
+	w.WriteHeader(http.StatusOK)
+
+	w.Write(audiodata)
 	log.Print(w, "Oracle query sent seccessfully.")
 }
